53a3b46cfa75854e81e572822df5f82e120105a5
Reinhard Hainz <reinhard.hainz@gmail.com>
2019-03-01 13:27:36 +0100

    rename pypi package to haip-config

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("Readme.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="haip_config",
-    version="0.1.6",
+    name="haip-config",
+    version="0.1.7",
     author="Reinhard Hainz",
     author_email="reinhard.hainz@gmail.com",
     description="A simple yaml config manager.",
